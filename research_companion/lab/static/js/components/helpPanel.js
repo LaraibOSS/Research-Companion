02850fe275a2ae8fb901ebb6c5c5d58e88190d5b
Laraib Hasan <Lxh417bham@gmail.com>
2026-07-09 10:15:52 +0100

    chore(release): bump version to 0.5.11

--- research_companion/lab/static/js/components/helpPanel.js
+++ research_companion/lab/static/js/components/helpPanel.js
@@ -12,7 +12,7 @@
 
 import { GLOSSARY } from '../glossary.js';
 
-const VERSION = 'v0.5.10';
+const VERSION = 'v0.5.11';
 const GITHUB_URL = 'https://github.com/Laraib-Hasan-Future/Research-Companion';
 const DOCS_PATH = 'docs/RESEARCH_COMPANION_GUIDE.pdf';
 
